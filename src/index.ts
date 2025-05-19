e44c60018480187d0c8f39bcb3b7cef5bb0af2d6
Mohamed-elamine-rabehi <mohamedelaminerabehi@gmail.com>
2025-05-19 20:37:53 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /** @format */
 
 import "dotenv/config";
+import cors from "cors";
 import express, { Request, Response } from "express";
 import apiRoute from "./routes";
 import ErrorController from "./controller/errorController";
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: '*', // allow requests from all origins (attacker's server)
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.get("/", (req, res, next) => {
   console.log("Hello, World!");
   res.status(200).json({ message: "Hello, World!" });
